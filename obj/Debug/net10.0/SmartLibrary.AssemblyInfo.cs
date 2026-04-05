76f65201a5240651a167cccd8e75e398e2c3bf6a
Amine <aminehafid098@gmail.com>
2026-04-05 11:50:28 +0100

    Add Borrow listing page

--- obj/Debug/net10.0/SmartLibrary.AssemblyInfo.cs
+++ obj/Debug/net10.0/SmartLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb3710e299302b740b0fd9aaf8b4a0412cdd7bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ccdcabccd29893b34abcc7121e3b4804b6fa82")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
